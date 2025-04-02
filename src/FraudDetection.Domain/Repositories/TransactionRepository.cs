d40c06965cba0f81ddc9bb5ff85c7a24f2841696
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-04-02 14:46:30 +0300

    refactor: remove GetAllTransactions

--- src/FraudDetection.Domain/Repositories/TransactionRepository.cs
+++ src/FraudDetection.Domain/Repositories/TransactionRepository.cs
@@ -1,11 +1,9 @@
-using System.Collections.Generic;
 using FraudDetection.Domain.Entities;
 
 namespace FraudDetection.Domain.Repositories
 {
   public interface ITransactionRepository
   {
-    IEnumerable<Transaction> GetAllTransactions();
     Transaction? GetById(string transactionID);
     IEnumerable<Transaction> StreamAllTransactions();
   }
